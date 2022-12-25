ae5c89c0eba52382e24351ce2e0f36fa83417170
NatLee <natlee.work@gmail.com>
2022-12-25 09:45:30 +0800

    [update] add `back to home`

--- main.py
+++ main.py
@@ -9,6 +9,23 @@ import numpy as np
 import mss
 import mouse
 
+func_buttons = ['home', 'advan']
+
+function_img_map = {}
+
+for func_button in func_buttons:
+    function_img_map[func_button] = {
+        'origin': f'./img/{func_button}-button.png',
+        'clicked': f'./img/{func_button}-clicked.png'
+    }
+
+def get_function_img_pattern(func):
+    origin = function_img_map[func]['origin']
+    origin_pattern = cv2.imread(origin)
+    clicked = function_img_map[func]['clicked']
+    clicked_pattern = cv2.imread(clicked)
+    return origin_pattern, clicked_pattern
+
 def get_screenshot(default_monitor_number=0):
     with mss.mss() as sct:
         monitor = sct.monitors[default_monitor_number] # 預設擷取全螢幕
@@ -46,8 +63,6 @@ def get_center_pt(*pt):
     x1, y1, x2, y2 = pt
     return ((x1+x2)//2, (y1+y2)//2)
 
-img_paths = [img for img in Path('./img').glob('*.png')]
-
 def wait_until_pattern(pattern, timeout):
     start_time = time.time()
     pts = []
@@ -60,34 +75,57 @@ def wait_until_pattern(pattern, timeout):
         pts = get_pts(screenshot, pattern)
     return pts, screenshot
 
-
-for img_path in img_paths:
-    pattern = cv2.imread(img_path.absolute().as_posix())
-    pts, screenshot = wait_until_pattern(pattern, timeout=3)
+def back_to_home() -> bool:
+    print("---- 執行回到主頁")
+    origin_pattern, clicked_pattern = get_function_img_pattern('home')
+    pts, screenshot = wait_until_pattern(origin_pattern, timeout=3)
     if pts:
-        print(f'---- Find [{img_path}]')
+        pt = pts[0]
+        x, y = get_center_pt(pt)
+        mouse_move(x, y)
+        return True
+
+    pts, screenshot = wait_until_pattern(clicked_pattern, timeout=1)
+    if pts:
+        print('---- 已經在Home頁面')
     else:
-        print(f'---- Timeout [{img_path}]')
-        continue
+        print(f'---- 此畫面找不到Home按鈕')
+    return False
 
-    pt = pts[0]
-    x1, y1, x2, y2 = pt
+def debug():
 
-    cv2.rectangle(
-        screenshot,
-        (x1, y1),
-        (x2, y2),
-        (0, 0, 255),
-        2
-    )
+    img_paths = [img for img in Path('./img').glob('*.png')]
 
-    center = get_center_pt(pt)
-    print(center)
-    x, y = center
-    mouse_move(x, y)
+    for img_path in img_paths:
+        pattern = cv2.imread(img_path.absolute().as_posix())
+        pts, screenshot = wait_until_pattern(pattern, timeout=3)
+        if pts:
+            print(f'---- Find [{img_path}]')
+        else:
+            print(f'---- Timeout [{img_path}]')
+            continue
 
-    screenshot_ = cv2.resize(screenshot, fx=0.3, fy=0.3, dsize=None)
-    cv2.imshow(f"{img_path}", screenshot_)
-    cv2.waitKey()
+        pt = pts[0]
+        x1, y1, x2, y2 = pt
 
+        cv2.rectangle(
+            screenshot,
+            (x1, y1),
+            (x2, y2),
+            (0, 0, 255),
+            2
+        )
+
+        center = get_center_pt(pt)
+        print(center)
+        x, y = center
+        mouse_move(x, y)
+
+        screenshot_ = cv2.resize(screenshot, fx=0.3, fy=0.3, dsize=None)
+        cv2.imshow(f"{img_path}", screenshot_)
+        cv2.waitKey()
+
+
+if __name__ == '__main__':
+    back_to_home()
 
