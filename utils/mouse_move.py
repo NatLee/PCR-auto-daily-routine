ff4cf89b5126cb96617f8cb49c8442ef5b1c5b3a
NatLee <natlee.work@gmail.com>
2023-01-14 20:20:47 +0800

    [update] add `waiting` for after and before
    Former-commit-id: 804d9315b36fc1aa5303cae2aba25008e7086fef

--- utils/mouse_move.py
+++ utils/mouse_move.py
@@ -1,10 +1,15 @@
-
+import time
 import mouse
 
-def mouse_move(x, y, click=True):
+def mouse_move(x, y, click=True, click_before_sec=0, click_after_sec=0):
     original_x, original_y = mouse.get_position()
     mouse.move(x, y, absolute=True)
     if click:
+        if click_before_sec:
+            time.sleep(click_before_sec)
         mouse.click(button='left')
     mouse.move(original_x, original_y, absolute=True)
+
+    if click_after_sec:
+        time.sleep(click_after_sec)
     return
